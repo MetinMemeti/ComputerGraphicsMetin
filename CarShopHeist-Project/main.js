7a210fe3b885a4660dfe1733459a6c5d1c104a2c
MetinMemeti <metinmemeti13@gmail.com>
2025-11-23 22:14:59 +0100

    Project Start

---
+++ CarShopHeist-Project/main.js
@@ -0,0 +1,6 @@
+import * as THREE from 'three';
+import { lightPosition } from 'three/src/nodes/TSL.js';
+import { color } from 'three/tsl';
+import { AmbientLight } from 'three/webgpu';
+import GUI from 'lil-gui'
+
